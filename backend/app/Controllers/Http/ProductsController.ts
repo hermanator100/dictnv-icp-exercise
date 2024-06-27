8aaa40388b5de9e2ae8eee4da1976d02660d06f0
hermanator100 <adverfuk.00090@gmail.com>
2024-06-27 23:23:09 +0800

    Finished the conversion

--- backend/app/Controllers/Http/ProductsController.ts
+++ backend/app/Controllers/Http/ProductsController.ts
@@ -76,53 +76,23 @@ export default class ProductsController {
         response.status(400);
         return response.json({
           status: 0,
-          message: 'User not found!',
+          message: 'Product not found!',
         });
-      }
-
-      if (bio) {
-        findProduct.bio = bio;
       }
 
       if (name) {
-        findUser.name = name;
+        findProduct.name = name;
       }
 
-      if (tiktok) {
-        findUser.tiktok = tiktok;
+      if (price) {
+        findProduct.price = price;
       }
 
-      if (twitter) {
-        findUser.twitter = twitter;
-      }
-
-      if (instagram) {
-        findUser.instagram = instagram;
-      }
-
-      if (facebook) {
-        findUser.facebook = facebook;
-      }
-
-      if (website) {
-        findUser.website = website;
-      }
-
-      if (profile_photo) {
-        findUser.profile_photo = profile_photo;
-      }
-
-      if (banner_photo) {
-        findUser.banner_photo = banner_photo;
-      }
-
-      findUser.updated_at = Date.now();
-
-      await findUser.save();
+      await findProduct.save();
 
       return response.json({
         status: 1,
-        message: 'User updated successfully!',
+        message: 'Product updated successfully!',
       });
     } catch (error: any) {
       response.status(400);
@@ -133,33 +103,4 @@ export default class ProductsController {
     }
   }
 
-  static async view_info_of_user_by_public(request: Request, response: Response) {
-    try {
-      const { username } = request.params;
-
-      const user = await User.findOneBy({
-        username,
-        status: 1,
-      });
-
-      if (!user) {
-        response.status(404);
-        return response.json({
-          status: 0,
-          message: 'User not found.',
-        });
-      }
-
-      return response.json({
-        status: 1,
-        data: user,
-      });
-    } catch (error: any) {
-      response.status(400);
-      return response.json({
-        status: 0,
-        message: error.message,
-      });
-    }
-  }
 }
